06d887eae90b502f03c89b054b75916fde3f1f9b
gushikem01 <gl_higaisha@yahoo.co.jp>
2024-10-01 22:11:36 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+import { renderer } from './renderer'
+
+const app = new Hono()
+
+app.use(renderer)
+
+app.get('/', (c) => {
+  return c.render(<h1>welcome to my page
+  </h1>)
+})
+
+export default app
